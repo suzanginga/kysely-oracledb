88d5d4dcd5310322b943912e044d359b2bc31c72
Joseph Tuffin <35460454+JosephTuffin@users.noreply.github.com>
2025-02-07 14:06:52 +0000

    updating config options for dialect and generator

--- src/dialect/driver.ts
+++ src/dialect/driver.ts
@@ -1,5 +1,5 @@
 import { DatabaseConnection, Driver } from "kysely";
-import { Connection, Pool } from "oracledb";
+import { Connection } from "oracledb";
 import { OracleConnection } from "./connection";
 import { OracleDialectConfig } from "./dialect";
 import { defaultLogger, Logger } from "./logger";
@@ -8,21 +8,17 @@ export class OracleDriver implements Driver {
     readonly #config: OracleDialectConfig;
     readonly #connections = new Map<string, OracleConnection>();
     readonly #log: Logger;
-    #pool?: Pool;
 
     constructor(config: OracleDialectConfig) {
         this.#config = config;
         this.#log = config.logger ? config.logger : defaultLogger;
     }
 
-    async init(): Promise<void> {
-        this.#pool =
-            typeof this.#config.pool === "function" ? await this.#config.pool({ alias: "kysely" }) : this.#config.pool;
-    }
+    async init(): Promise<void> {}
 
     async acquireConnection(): Promise<DatabaseConnection> {
         this.#log.trace("Acquiring connection");
-        const connection = new OracleConnection((await this.#pool?.getConnection()) as Connection, this.#log);
+        const connection = new OracleConnection((await this.#config.pool?.getConnection()) as Connection, this.#log);
         this.#connections.set(connection.identifier, connection);
         return connection;
     }
@@ -53,6 +49,6 @@ export class OracleDriver implements Driver {
         for (const connection of this.#connections.values()) {
             await this.releaseConnection(connection as OracleConnection);
         }
-        await this.#pool?.close();
+        await this.#config.pool?.close();
     }
 }
