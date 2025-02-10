db4fdc67a50ea40ec0bd169ee5e2db5b94f3dee1
Joseph Tuffin <35460454+JosephTuffin@users.noreply.github.com>
2025-02-10 11:48:17 +0000

    added fix for camel case util bug

--- src/generator/utils.ts
+++ src/generator/utils.ts
@@ -1,4 +1,4 @@
 export const camelCase = (str: string) =>
-    str.toLowerCase().replace(/([-_][a-z])/g, (group) => group.toUpperCase().replace("-", "").replace("_", ""));
+    str.toLowerCase().replace(/([-_][a-z0-9])/g, (group) => group.toUpperCase().replace("-", "").replace("_", ""));
 
-export const pascalCase = (str: string) => camelCase(str).replace(/^[a-z]/, (char) => char.toUpperCase());
+export const pascalCase = (str: string) => camelCase(str).replace(/^[a-z0-9]/, (char) => char.toUpperCase());
