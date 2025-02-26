03779245a0478e8f238d29e39a529e7cc9c64470
Joseph Tuffin <35460454+JosephTuffin@users.noreply.github.com>
2025-02-26 11:18:51 +0000

    added adapter unit tests

--- src/__mocks__/oracledb.ts
+++ src/__mocks__/oracledb.ts
@@ -1,7 +1,23 @@
-import { Pool } from "oracledb";
+import { Connection, Pool } from "oracledb";
+import { vi } from "vitest";
 
 export default {
     createPool: async (): Promise<Pool> => {
-        return {} as Pool;
+        return {
+            getConnection: async () => {
+                return Promise.resolve({
+                    close: vi.fn() as unknown as Connection["close"],
+                    commit: vi.fn() as unknown as Connection["commit"],
+                    rollback: vi.fn() as unknown as Connection["rollback"],
+                    execute: vi.fn(() => {
+                        return {
+                            rows: [],
+                            rowsAffected: 0,
+                        };
+                    }) as unknown as Connection["execute"],
+                } as Connection);
+            },
+            close: vi.fn() as unknown as Pool["close"],
+        } as Pool;
     },
 };
